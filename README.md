6a3f47be72c07b465811895a67bd4b0f263aac9d
Tamin Choudhury <taminc@live.com>
2022-04-13 00:17:29 +0100

    changed code and updated README file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# 4-Code-Quiz
+# 4-Code-Quiz#
+
+In this project, I have created an application that starts a coding quiz. There are functions to start a countdown timer (which decreases with wrong answers), set a random question from an array and saves the score once the quiz is finished.
+
+[Application] (https://tamin96.github.io/4-Code-Quiz/)
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,43 +1,75 @@
-var startButton = document.querySelector("#start");
-var timerEl = document.querySelector(".timer");
+var startButton = document.getElementById("start");
+var timerEl = document.getElementById("time");
+var startScreen = document.getElementById("start-screen");
+var questionTitle = document.getElementById("question-title");
+var choiceEl = document.getElementById("choices");
 
-startButton.addEventListener("click", function() {
+var i = 0
+var correctAnswer = questions[i].answer 
+
+function startQuiz() {
+  console.log("Quiz started")
   countdown();
-  setQuestion();
-});
-
-var score = 0;
-
-function setScore() {
-
-};
+  getNextQuestion();
+  setChoices();
+}
 
 function countdown() {
-    var timeLeft = 60;
+  var timeLeft = 60;
   
-    var timeInterval = setInterval(function () {
+  var timeInterval = setInterval(function () {
+    
+    if (timeLeft > 1) {
 
-      if (timeLeft > 1) {
-
-        timerEl.textContent = 'Time: ' + timeLeft;
-
+        timerEl.textContent = timeLeft;
         timeLeft--;
       } else if (timeLeft === 1) {
 
-        timerEl.textContent = 'Time: ' + timeLeft;
+        timerEl.textContent = timeLeft;
         timeLeft--;
       } else {
 
         timerEl.textContent = '';
-
         clearInterval(timeInterval);
-
-        displayMessage();
       }
     }, 1000);
-  };
+}
 
-  function setQuestion() {
+function getNextQuestion() {
+  startScreen.textContent = "";
 
-  };
-  
+  if (i < questions.length) {
+    questionTitle.textContent = questions[i].title;
+  }
+}
+
+function setChoices() {
+  var choice1 = document.createElement("button");
+  var choice2 = document.createElement("button");
+  var choice3 = document.createElement("button");
+  var choice4 = document.createElement("button");
+
+  choiceEl.appendChild(choice1);
+  choiceEl.appendChild(choice2);
+  choiceEl.appendChild(choice3);
+  choiceEl.appendChild(choice4);
+
+  choice1.textContent = questions[i].choices[1];
+  choice2.textContent = questions[i].choices[2];
+  choice3.textContent = questions[i].choices[3];
+  choice4.textContent = questions[i].choices[4];
+
+  choiceEl.addEventListener("click", function(e){
+    var clickElement = e.target.textContent
+
+    if (clickElement === correctAnswer){
+      console.log("Correct answer")
+      return
+    } else console.log ("Wrong answer")
+
+
+  });
+
+}
+
+startButton.addEventListener("click", startQuiz);
